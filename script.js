ea4f3ec0a8d2427a55afb4da78d86aa0ea012566
Adepoju Ololade Joshua Ilerioluwa <138535455+Ileri2020@users.noreply.github.com>
2024-04-27 00:04:10 +0100

    Add files via upload
    mobile font size update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const side_toggle = document.getElementById("side_toggle");
 const sidebar = document.getElementById("sidebar_container");
 const profile_img2 = document.getElementById("profile_img2");
+const main_cont = document.getElementById("main_cont");
 
 side_toggle.addEventListener("click", () => {
     sidebar.classList.toggle("hide");
     profile_img2.classList.toggle("show");
+})
+
+main_cont.addEventListener("click", () => {
+    if(!sidebar.classList.contains("hide")){
+        sidebar.classList.add("hide");
+        profile_img2.classList.toggle("show");
+    }  
 })
