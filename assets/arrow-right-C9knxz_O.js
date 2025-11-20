1c51e95c1ddbae3293831229a8a70e411eef7ff9
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2025-11-20 13:09:24 +0100

    Updates

--- assets/arrow-right-C9knxz_O.js
+++ assets/arrow-right-C9knxz_O.js
@@ -1,4 +1,4 @@
-import{c as r}from"./main-CU2HIAwf.js";/**
+import{c as r}from"./main-CCHWjm8a.js";/**
  * @license lucide-react v0.414.0 - ISC
  *
  * This source code is licensed under the ISC license.
